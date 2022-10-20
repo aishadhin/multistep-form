471181ef42bfa1344d42856b2858f0e53d9c1bf4
aishadin <aishadhin.ac@gmail.com>
2022-10-20 13:14:49 +0600

    step customize

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Details from "./Components/Steps/Details";
 import Final from "./Components/Steps/Final";
 
 function App() {
-
   const [currentStep, setCurrentStep] = useState(1);
 
   const steps = ["Account Information", "Personal Details", "Complete"];
@@ -24,16 +23,26 @@ function App() {
     }
   };
 
+  const handleClick = (direction) => {
+    let newStep = currentStep;
+    direction === "next" ? newStep++ : newStep--;
+    newStep > 0 && newStep <= steps.length && setCurrentStep(newStep);
+  };
+
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       {/* Stepper  */}
 
       <div className="container horizontal mt-5">
-        <Stepper steps = {steps} currentStep = {currentStep}></Stepper>
+        <Stepper steps={steps} currentStep={currentStep}></Stepper>
       </div>
 
       {/* Navigation Controls */}
-      <StepperControl></StepperControl>
+      <StepperControl
+        handleClick={handleClick}
+        currentStep={currentStep}
+        steps={steps}
+      ></StepperControl>
     </div>
   );
 }
--- src/Components/Stepper.js
+++ src/Components/Stepper.js
@@ -67,15 +67,32 @@ const Stepper = ({ steps, currentStep }) => {
         }
       >
         <div className="relative flex flex-col items-center text-teal-600">
-          <div className="rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3">
-            {/* Display Number  */}1
+          <div
+            className={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3 ${
+              step?.selected
+                ? "bg-green-600 text-white font-bold border border-green-600"
+                : ""
+            }`}
+          >
+            {/* Display Number  */}
+            {step.completed ? (
+              <span className="text-white font-bold text-xl">&#10003;</span>
+            ) : (
+              index + 1
+            )}
           </div>
-          <div className="absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase">
+          <div
+            className={`absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase ${
+              step?.highlighted ? "text-gray-900" : "text-gray-400"
+            }`}
+          >
             {/* Display Description  */}
-            Description
+            {step?.description}
           </div>
         </div>
-        <div className="flex-auto border-t-2 transition duration-500 ease-in-out">
+        <div
+          className={`flex-auto border-t-2 transition duration-500 ease-in-out ${step.completed ? "border-green-600" : "border-gray-300"}`}
+        >
           {/* Display Line  */}
         </div>
       </div>
