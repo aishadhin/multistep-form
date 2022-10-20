85e0b3294fd9b3faefd2c7505f5ff7bdb7fe667e
aishadin <aishadhin.ac@gmail.com>
2022-10-20 12:00:01 +0600

    step create

--- src/Components/Stepper.js
+++ src/Components/Stepper.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Stepper = () => {
-    return (
-        <div>
-            
+  const displaySteps = (
+    <div className="w-full flex items-center">
+      <div className="relative flex flex-col items-center text-teal-600">
+        <div className="rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3">{/* Display Number  */}
+        1
         </div>
-    );
+        <div className="absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase">{/* Display Description  */}
+        Description
+        </div>
+      </div>
+      <div className="flex-auto border-t-2 transition duration-500 ease-in-out">{/* Display Line  */}</div>
+    </div>
+  )
+
+
+  return (
+    <div className="flex items-center justify-between mx-4">
+      {displaySteps}
+    </div>
+  );
 };
 
 export default Stepper;
